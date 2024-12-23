d2d8762886e46d5c694d5350fd54413696e49848
dreamProject <arunkumar.srt@gmail.com>
2024-12-23 18:06:30 +0530

    Project created

---
+++ TMDBTest/TMDBTest/TMDBTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBTestApp.swift
+//  TMDBTest
+//
+//  Created by Arunkumar K M on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TMDBTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
